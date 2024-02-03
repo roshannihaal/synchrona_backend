c4b25db74268f245ff2f73c5a89368965668aeab
roshannihaal <roshan.nihaal18@gmail.com>
2024-02-03 19:35:36 +0530

    Dockerized Application

--- src/app.ts
+++ src/app.ts
@@ -6,4 +6,11 @@ const app = express()
 const server = http.createServer(app)
 const io = new Server(server)
 
+app.get('/', (req, res) => {
+  const resStatusCode = 200
+  return res
+    .status(resStatusCode)
+    .send({ statusCode: resStatusCode, message: 'Hello World!' })
+})
+
 export { io, server }
