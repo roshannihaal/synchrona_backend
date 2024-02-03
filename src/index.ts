284c04deba3a91b6066c50033c2d421ab5fea733
roshannihaal <roshan.nihaal18@gmail.com>
2024-02-03 19:24:04 +0530

    Init

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { io, server } from './app'
+import { config } from './config'
+
+const port = config.PORT
+const node_env = config.NODE_ENV
+server.listen(port, () => {
+  console.log(`App (${node_env}) listening at http://localhost:${port}`)
+})
+
+io.on('connection', (socket) => {
+  console.log(socket.id)
+  console.log('a user connected')
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+})
