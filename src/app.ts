e99204600a5ab248a9420d7195e224efd9ece863
roshannihaal <roshan.nihaal18@gmail.com>
2024-06-09 11:03:59 +0530

    added cors to sockets

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,11 @@ import express from 'express'
 import http from 'http'
 import { Server } from 'socket.io'
 import { socketClientService } from './service'
+import { config } from './config'
 
 const app = express()
 const server = http.createServer(app)
-const io = new Server(server)
+const io = new Server(server, { cors: { origin: config.FRONTEND_URL } })
 
 try {
   socketClientService.initializeSockets()
